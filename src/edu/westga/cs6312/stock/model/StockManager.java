267bcafa7334c4a334a23d2e3e57fda89333f53b
James Luke Johnson <jjohn144@my.westga.edu>
2018-04-22 11:53:37 -0500

    Initial Commit. Created packages and model classes.

---
+++ src/edu/westga/cs6312/stock/model/StockManager.java
@@ -0,0 +1,12 @@
+package edu.westga.cs6312.stock.model;
+
+/**
+ * Defines the StockManager class, which will keep up with 
+ * one or more historical records about a particular stock. 
+ * 
+ * @author James Luke Johnson
+ * @version 2018.04.22
+ */
+public class StockManager {
+
+}
---
+++ src/edu/westga/cs6312/stock/model/StockRecord.java
@@ -0,0 +1,12 @@
+package edu.westga.cs6312.stock.model;
+
+/**
+ * Defines the StockRecord class, which will keep up with information 
+ * about a particular stock on a given day. 
+ * 
+ * @author James Luke Johnson
+ * @version 2018.04.22
+ */
+public class StockRecord {
+
+}

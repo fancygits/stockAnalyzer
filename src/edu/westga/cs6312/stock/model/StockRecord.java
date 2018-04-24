1fe44614df61ea3ba4f45abf93ca066d5da97924
James Luke Johnson <jjohn144@my.westga.edu>
2018-04-23 20:58:07 -0500

    Added Javadoc comments for StockRecord

--- src/edu/westga/cs6312/stock/model/StockRecord.java
+++ src/edu/westga/cs6312/stock/model/StockRecord.java
@@ -20,6 +20,16 @@ public class StockRecord {
 	private double adjClose;
 	private long volume;
 
+	/**
+	 * Constructs a new StockRecord object
+	 * @param strDate	A string representing a date in the format MM/dd/yyyy
+	 * @param open		The opening stock price
+	 * @param high		The high stock price
+	 * @param low		The low stock price
+	 * @param close		The closing stock price
+	 * @param adjClose	The adjusted closing stock price
+	 * @param volume	The volume of stocks bought and sold
+	 */
 	public StockRecord(String strDate, double open, double high, double low, double close, double adjClose, long volume) {
 		if (open < 0 | high < 0 | low < 0 | close < 0 | adjClose < 0 | volume < 0) {
 			throw new IllegalArgumentException("Stock numbers must be positive");
@@ -46,40 +56,76 @@ public class StockRecord {
 		
 	}
 	
+	/** 
+	 * Returns a string representation of the object
+	 */
+	@Override
 	public String toString() {
 		String output = this.dateReformatter(this.date);
 		output += String.format("%11.2f%11.2f%11.2f%11.2f%11.2f%10d", this.open, this.high, this.low, this.close, this.adjClose, this.volume);
 		return output;
 	}
 	
+	/**
+	 * Accessor for the date in the format MM/dd/yyyy
+	 * @return	String date
+	 */
 	public String getDate() {
 		return this.date;
 	}
 	
+	/**
+	 * Accessor for the reformatted date in the format yyyy-MM-dd
+	 * @return	String reformatted date
+	 */
 	public String getReformattedDate() {
 		return this.dateReformatter(this.date);
 	}
 	
+	/**
+	 * Accessor for the opening price
+	 * @return	The opening price
+	 */
 	public double getOpen() {
 		return this.open;
 	}
-	
+
+	/**
+	 * Accessor for the high price
+	 * @return	The high price
+	 */
 	public double getHigh() {
 		return this.high;
 	}
-	
+
+	/**
+	 * Accessor for the low price
+	 * @return	The low price
+	 */
 	public double getLow() {
 		return this.low;
 	}
-	
+
+	/**
+	 * Accessor for the closing price
+	 * @return	The closing price
+	 */
 	public double getClose() {
 		return this.close;
 	}
-	
+
+	/**
+	 * Accessor for the adjusted closing price
+	 * @return	The adjusted closing price
+	 */
 	public double getAdjClose() {
 		return this.adjClose;
 	}
-	
+
+	/**
+	 * Accessor for the volume sold
+	 * @return	The volume
+	 */
 	public long getVolume() {
 		return this.volume;
 	}
